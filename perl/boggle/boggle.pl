89cb2145eeefe621e6291ec3387b979bb65a6124
Greg Sterling <gregory.sterling@me.com>
2023-11-30 06:54:46 -0500

    run dos2unix, add /usr/bin/perl header, add strict and warnings

--- perl/boggle/boggle.pl
+++ perl/boggle/boggle.pl
@@ -1,20 +1,24 @@
-@dice = qw(foxrib moqabj furilw setupl camped acitao slcrae romash 
-		   nodesw hefiye onudtk tevign anedvz pinesh abilyt gkyleu);
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my @dice = qw(foxrib moqabj furilw setupl camped acitao slcrae romash nodesw hefiye onudtk tevign anedvz pinesh abilyt gkyleu);
 
 my @board = &make_board(fy_shuffle(@dice));
-$board = join "", @board;
+my $board = join "", @board;
 $board =~ s/([^\n]{4})/$1\n/g;
 
 print $board;
 
 sub make_board {
-	my @board, $i;
-	$board[$i++] = substr $_, int rand length, 1 foreach @_;
-	return @board;
+    my @board;
+    my $i;
+    $board[$i++] = substr $_, int rand length, 1 foreach @_;
+    return @board;
 } # make_board
 
 sub fy_shuffle {
-	my $i = @_;
-	@_[$i, $_] = @_[$_, $i] for int rand(--$i + 1);
-	return @_;
+    my $i = @_;
+    @_[$i, $_] = @_[$_, $i] for int rand(--$i + 1);
+    return @_;
 } # fy_shuffle
